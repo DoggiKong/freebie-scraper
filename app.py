227f0e2a8a99f92d0cd9a8179f84a68ddc1affd8
Jerry Wang <jerryw4113@gmail.com>
2020-06-12 18:26:06 +1000

    Refactored Ozbargain URL

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-node_url = "https://www.ozbargain.com.au"
-soup = BeautifulSoup(requests.get("https://www.ozbargain.com.au/freebies").text, "html.parser")
+ozbargain_url = "https://www.ozbargain.com.au"
+soup = BeautifulSoup(requests.get(ozbargain_url + "/freebies").text, "html.parser")
 
 for item_node in soup.find_all("div", class_="node-ozbdeal"):
     json_dict = {
             "title": item_node.find("h2", class_="title").text,
             "submitted": item_node.find("div", class_="submitted").text,
-            "url": node_url + item_node.find("h2", class_="title").find("a", href=True)["href"],
+            "url": ozbargain_url + item_node.find("h2", class_="title").find("a", href=True)["href"],
             "description": item_node.find("div", class_="content").text.strip("\n ")
     }
 
